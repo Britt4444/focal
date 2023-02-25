f48db7ff7ac588fa0f9a6a5c087d4730c679c38a
Brittany Cowper <brittferren@gmail.com>
2023-02-25 13:50:26 -0600

    Dice-roller function created with helper function

--- dice-roller.js
+++ dice-roller.js
@@ -1,15 +1,20 @@
-//return a random dice roll each time from 1-6
-const rollResult = function () {
+//function to return a random dice roll each time from 1-6
+const rollResult = function() {  
   return Math.floor((Math.random() * 6) + 1);
-}
+};
+//slice out index 0 and 1 
 const numOfRolls = process.argv.slice(2);
 
-const rollDie = function (n) {
+const rollDie = function() {
   let result = [];
+  //must use numOfRolls rather than numOfRolls.length, which will always be 1 here
   for (let i = 0; i < numOfRolls; i++) {
+  //for each numOfRolls, return result of rollResult
     result += (rollResult());
   }
-  return result; 
+  //split into single digits, add comma and space
+  result = ("" + result).split("").join(", ");
+  return result;
 };
 
 console.log(`Rolled ${numOfRolls} dice: ${rollDie(numOfRolls)}`);
